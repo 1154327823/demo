791d2f87bbd60eeccbdb401125dc5d16f56714e6
53985606 <53985606@qq.com>
2020-12-01 10:55:11 +0800

    demo

---
+++ src/com/bjpowernode/test.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+/**
+ * @Author LGR
+ * @Date 2020/12/1 10:53
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
